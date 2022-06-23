d96097d9204ae7c957a0f251f20c3de353dd0fb5
alanaafsc <alana.ingrid@ufpe.br>
2022-06-22 21:30:18 -0300

    Projeto do contador

---
+++ projeto_contador/assets/js/script2.js
@@ -0,0 +1,19 @@
+//TESTE addEventListener() método - MANIPULADOR DE EVENTOS
+let count = 0;
+
+//const CURRENT_NUMBER = document.getElementById('currentNumber');
+//CURRENT_NUMBER = target.addEventListener('click', function, useCapture);
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+} 

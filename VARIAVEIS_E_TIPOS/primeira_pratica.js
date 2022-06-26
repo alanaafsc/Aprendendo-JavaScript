73c264eb9d24b444f7cddf3a0c9e3d463d1099a1
alanaafsc <alana.ingrid@ufpe.br>
2022-06-26 17:04:47 -0300

    Variáveis e tipos no js: atividade do palíndromo e dos números pares

---
+++ VARIAVEIS_E_TIPOS/primeira_pratica.js
@@ -0,0 +1,22 @@
+//PALÍNDROMO 
+//solução 1:
+function verificaPalindromo(string){
+    if(!string) return "string inexistente";
+
+    return string.split("").reverse().join("") === string; //verifica se string invertida = string que funcao recebeu
+}
+
+//console.log(verificaPalindromo("gato"));
+
+// Solução 2:
+function verificaPalindromo2(string){
+    if (!string) return "string inexistente";
+
+    for(let i = 0; i < string.length / 2; i++){
+        if(string[i] !== string[string.length - 1 - i]){
+        return false; }
+    }
+    return true
+}
+
+console.log(verificaPalindromo("abba"));
